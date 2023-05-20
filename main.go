efe7d75590bd81248c23306c6d7c40329af45c96
Oğuzhan Kazak <oguzhankazak@outlook.com>
2023-05-20 17:55:13 +0300

    remote mouse control over webpage using websocket technology initial implementation

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // İstemci kaynaklı doğrulamayı devre dışı bırakmak için true döndürün.
+	},
+}
+
+func main() {
+	http.HandleFunc("/ws", handleWebSocket)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer conn.Close()
+
+	// İstemciden mesajları almak için sonsuz bir döngü
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		var receivedCoordinate POINT
+		json.Unmarshal([]byte(msg), &receivedCoordinate)
+		log.Printf("received X: %d received Y: %d", receivedCoordinate.X, receivedCoordinate.Y)
+		SetCursorPosition(receivedCoordinate.X, receivedCoordinate.Y)
+	}
+}
---
+++ mouse.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"log"
+	"syscall"
+	"unsafe"
+)
+
+var (
+	user32       = syscall.NewLazyDLL("user32.dll")
+	setCursorPos = user32.NewProc("SetCursorPos")
+	getCursorPos = user32.NewProc("GetCursorPos")
+	mouseEvent   = user32.NewProc("mouse_event")
+)
+
+const (
+	MOUSEEVENTF_LEFTDOWN   = 0x0002
+	MOUSEEVENTF_LEFTUP     = 0x0004
+	MOUSEEVENTF_RIGHTDOWN  = 0x0008
+	MOUSEEVENTF_RIGHTUP    = 0x0010
+	MOUSEEVENTF_MIDDLEDOWN = 0x0020
+	MOUSEEVENTF_MIDDLEUP   = 0x0040
+)
+
+type POINT struct {
+	X int `json:"x"`
+	Y int `json:"y"`
+}
+
+func SetCursorPosition(x, y int) {
+	log.Printf("Setting position: x: %d y: %d", x, y)
+	setCursorPos.Call(uintptr(x), uintptr(y))
+}
+
+func GetCursorPosition() (int, int) {
+	var point POINT
+	getCursorPos.Call(uintptr(unsafe.Pointer(&point)))
+	return int(point.X), int(point.Y)
+}
+
+func LeftClick() {
+	mouseEvent.Call(
+		uintptr(MOUSEEVENTF_LEFTDOWN),
+		0,
+		0,
+		0,
+		0,
+	)
+	mouseEvent.Call(
+		uintptr(MOUSEEVENTF_LEFTUP),
+		0,
+		0,
+		0,
+		0,
+	)
+}
